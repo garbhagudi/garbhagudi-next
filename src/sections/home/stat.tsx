5e7920308b7bd87f267186a555bc385e66617443
garbhagudi <garbhagudi.11@gmail.com>
2022-06-07 17:51:38 +0530

    Stat Page Changes

--- src/sections/home/stat.tsx
+++ src/sections/home/stat.tsx
@@ -5,71 +5,99 @@ const metrics = [
     id: 1,
     stat: "7000+",
     emphasis: "Happy Families",
+    bg: "bg-brandPink4",
   },
   {
     id: 2,
     stat: "111+",
     emphasis: "Free Fertility Awareness Camps",
+    bg: "bg-brandYellow",
   },
   {
     id: 3,
     stat: "65-73%",
     emphasis: "IVF Success Rate",
+    bg: "bg-brandPurple",
   },
   {
     id: 4,
     stat: "5",
     emphasis: "Centres Across Bangalore",
+    bg: "bg-cyan2",
+  },
+  {
+    id: 1,
+    stat: "7000+",
+    emphasis: "Happy Families",
+    bg: "bg-brandPurple2",
+  },
+  {
+    id: 2,
+    stat: "111+",
+    emphasis: "Free Fertility Awareness Camps",
+    bg: "bg-brandPink3",
+  },
+  {
+    id: 3,
+    stat: "65-73%",
+    emphasis: "IVF Success Rate",
+    bg: "bg-cyan3",
+  },
+  {
+    id: 4,
+    stat: "5",
+    emphasis: "Centres Across Bangalore",
+    bg: "bg-blue3",
+  },
+  {
+    id: 4,
+    stat: "5",
+    emphasis: "Centres Across Bangalore",
+    bg: "bg-yellow3",
   },
 ];
 
-const Stat = () => {
+export const Statistic = () => {
   return (
-    <div className="relative max-w-7xl mx-auto">
-      <div className="h-80 w-full absolute bottom-0 xl:inset-0 xl:h-full">
-        <div className="h-full w-full xl:grid xl:grid-cols-2">
-          <div className="h-full xl:relative xl:col-start-2">
-            <img
-              className="h-full w-full object-cover opacity-70 xl:absolute xl:inset-0"
-              src="https://res.cloudinary.com/garbhagudiivf/image/upload/v1643272924/Home/Our_never_ending_s0dbde.webp"
-              alt="People working on laptops"
-            />
-            <div
-              aria-hidden="true"
-              className="absolute inset-x-0 top-0 h-32 bg-gradient-to-b from-white to-transparent xl:inset-y-0 xl:left-0 xl:h-full xl:w-32 xl:bg-gradient-to-r"
-            />
+    <div
+      className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full md:px-24 lg:px-8 lg:py-20 bg-[url('https://res.cloudinary.com/garbhagudiivf/image/upload/v1654599843/Home/5570863-min_qd5rpq.jpg')] bg-contain"
+      id="stats"
+    >
+      <div className="grid gap-24 row-gap- lg:grid-cols-5 max-w-7xl mx-auto">
+        <div className="grid gap-8 lg:col-span-2">
+          <div className="flex flex-col items-center justify-center">
+            <p className="mb-2 text-2xl lg:text-4xl font-bold font-heading">
+              Our Ever-Growing Success
+            </p>
+            <p className="text-gray-900 font-content mt-4 text-lg">
+              GarbhaGudi is a place where dreams come alive, hopes never fade,
+              and possibilities never end. We ceaselessly work to help you
+              cherish the golden moment of holding your little bundles of joy.
+              Along with our quality of treatment and care, some of the other
+              factors that sets us apart from our peers are:
+            </p>
           </div>
         </div>
-      </div>
-      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8 xl:grid xl:grid-cols-2 xl:grid-flow-col-dense xl:gap-x-8">
-        <div className="relative pt-12 pb-64 sm:pt-24 sm:pb-64 xl:col-start-1 xl:pb-24">
-          <p className="mt-3 text-3xl font-extrabold text-brandDark font-heading">
-            Our Ever-Growing Success
-          </p>
-          <p className="mt-5 text-lg text-brandDark font-content">
-            GarbhaGudi is a place where dreams come alive, hopes never fade, and
-            possibilities never end. We ceaselessly work to help you cherish the
-            golden moment of holding your little bundles of joy. Along with our
-            quality of treatment and care, some of the other contributing
-            factors for achieving high success rates are:
-          </p>
-          <div className="mt-12 grid grid-cols-1 gap-y-12 gap-x-6 sm:grid-cols-2">
-            {metrics.map((item) => (
-              <p key={item.id}>
-                <span className="block text-2xl font-bold text-brandDark">
-                  {item.stat}
-                </span>
-                <span className="mt-1 block text-base text-brandliteGray">
-                  <span className="font-medium text-brandDark font-content">
-                    {item.emphasis}
-                  </span>{" "}
-                </span>
-              </p>
-            ))}
-          </div>
+        <div className="grid rounded lg:col-span-3 grid-cols-3 lg:grid-cols-3">
+          {metrics.map((items) => (
+            <div
+              className={`flex flex-col justify-between rounded-lg p-8 sm:p-10 border hover:${items.bg} transition-all `}
+              key={items?.id}
+            >
+              <div className="flex flex-col items-center justify-center">
+                <div className="text-lg font-bold text-gray-800 sm:text-2xl">
+                  {items?.stat}
+                </div>
+                <div className="text-sm text-gray-800 sm:text-base sm:font-semibold text-center">
+                  {items?.emphasis}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
-export default Stat;
+
+export default Statistic;
