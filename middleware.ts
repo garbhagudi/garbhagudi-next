97255cb97d1c5f7218bcacd6da63b1265cfb6948
garbhagudi <garbhagudi.11@gmail.com>
2022-10-12 18:25:08 +0530

    middleware fix 2

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl.clone();
-  if (pathname == "/locations/south-end-circle") {
+  if (pathname === "/locations/south-end-circle") {
     return NextResponse.redirect("/locations/jayanagar");
   }
   return NextResponse.next();
