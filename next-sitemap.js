2769158da7075f7cc1a1e52f778d08180967f888
garbhagudi <garbhagudi.11@gmail.com>
2023-01-04 14:20:39 +0530

    sitemnap fix - 2

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,7 +3,6 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.garbhagudi.com",
   generateRobotsTxt: true,
-  changefreq: daily,
   sitemapSize: 5000,
   generateIndexSitemap: false,
   priority: "0.8",
