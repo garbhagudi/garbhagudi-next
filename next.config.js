636bbaa038a3234102f40b6aec2e1e5019b9cfc8
garbhagudi <garbhagudi.11@gmail.com>
2023-02-20 15:33:57 +0530

    404 redirects fix

--- next.config.js
+++ next.config.js
@@ -75,122 +75,117 @@ const nextConfig = {
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/feed/",
+        source: "/feed/",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/tag/ovarian-response/",
+        source: "/tag/ovarian-response",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/category/garbhagudi_ivf/",
+        source: "/category/garbhagudi_ivf",
         destination: "/treatments/in-vitro-fertilization-ivf",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/profile/dr-asha-s-vijay/",
+        source: "/profile/dr-asha-s-vijay",
         destination: "/fertility-experts/dr-asha-s-vijay",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/events/feed/",
+        source: "/events/feed",
         destination: "/",
         permanent: true,
       },
       {
-        source:
-          "https://www.garbhagudi.com/endocrine-disruptors-and-fertility/",
+        source: "/endocrine-disruptors-and-fertility",
         destination: "/blogs/page/1",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/events/2016-05-24/?ical=1/",
+        source: "/events/2016-05-24/?ical=1",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/what-is-iui-intrauterine-",
+        source: "/what-is-iui-intrauterine-",
         destination: "/blogs/page/1",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/how-to-improve-",
+        source: "/how-to-improve-",
         destination: "/blogs/page/1",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/page/6/?s=IUI&submit=Go",
+        source: "/page/6/?s=IUI&submit=Go",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/events/2016-07-23/?ical=1/",
+        source: "/events/2016-07-23/?ical=1",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/events/2016-05-29/",
+        source: "/events/2016-05-29",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/events/2017-12/",
+        source: "/events/2017-12",
         destination: "/",
         permanent: true,
       },
       {
-        source:
-          "https://www.garbhagudi.com/know-when-icsi-or-ivf-is-preferred-in-infertility-treatment/",
+        source: "/know-when-icsi-or-ivf-is-preferred-in-infertility-treatment",
         destination: "/blogs/page/1",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/contact-2/",
+        source: "/contact-2",
         destination: "/gg-care",
         permanent: true,
       },
       {
-        source:
-          "https://www.garbhagudi.com/features/Success%20Rates%20of%20IVF",
+        source: "/features/Success%20Rates%20of%20IVF",
         destination: "/features/success-rates-of-ivf",
         permanent: true,
       },
       {
-        source:
-          "https://www.garbhagudi.com/features/World%20Class%20Fertility%20Care",
+        source: "/features/World%20Class%20Fertility%20Care",
         destination: "/features/world-class-fertility-care",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/features/Affordable%20Treatments",
+        source: "/features/Affordable%20Treatments",
         destination: "/features/affordable-treatments",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/features/Financing%20Options",
+        source: "/features/Financing%20Options",
         destination: "/features/financing-options",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/features/Holistic%20Approach",
+        source: "/features/Holistic%20Approach",
         destination: "/features/holistic-approach",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/features/Treatment%20Protocols",
+        source: "/features/Treatment%20Protocols",
         destination: "/features/treatment-protocols",
         permanent: true,
       },
       {
-        source:
-          "https://www.garbhagudi.com/features/Best%20in%20Class%20Facilities",
+        source: "/features/Best%20in%20Class%20Facilities",
         destination: "/",
         permanent: true,
       },
       {
-        source: "https://www.garbhagudi.com/about/",
+        source: "/about/",
         destination: "/about/overview",
         permanent: true,
       },
