1f31c9c32aabe8b1807687f816473e2bf831f772
garbhagudi <garbhagudi.11@gmail.com>
2023-05-05 12:35:00 +0530

    minor changes

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -168,112 +168,112 @@ const Blog = ({ blog }) => {
         link4={""}
         text4={""}
       />
-      <div className="relative py-16 bg-white overflow-hidden">
-        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-          <div
-            className="relative h-full text-lg max-w-prose mx-auto"
-            aria-hidden="true"
-          >
-            <svg
-              className="absolute top-12 left-full transform translate-x-32"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
+      {blog?.slug ? (
+        <div className="relative py-16 bg-white overflow-hidden">
+          <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+            <div
+              className="relative h-full text-lg max-w-prose mx-auto"
+              aria-hidden="true"
             >
-              <defs>
-                <pattern
-                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
+              <svg
+                className="absolute top-12 left-full transform translate-x-32"
                 width={404}
                 height={384}
-                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
-              />
-            </svg>
-            <svg
-              className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-            >
-              <defs>
-                <pattern
-                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
+                fill="none"
+                viewBox="0 0 404 384"
+              >
+                <defs>
+                  <pattern
+                    id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
                     x={0}
                     y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-100"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={384}
+                  fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+                />
+              </svg>
+              <svg
+                className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
                 width={404}
                 height={384}
-                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-              />
-            </svg>
-            <svg
-              className="absolute bottom-12 left-full transform translate-x-32"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-            >
-              <defs>
-                <pattern
-                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
+                fill="none"
+                viewBox="0 0 404 384"
+              >
+                <defs>
+                  <pattern
+                    id="f210dbf6-a58d-4871-961e-36d5016a0f49"
                     x={0}
                     y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-100"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={384}
+                  fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+                />
+              </svg>
+              <svg
+                className="absolute bottom-12 left-full transform translate-x-32"
                 width={404}
                 height={384}
-                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
-              />
-            </svg>
+                fill="none"
+                viewBox="0 0 404 384"
+              >
+                <defs>
+                  <pattern
+                    id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect
+                      x={0}
+                      y={0}
+                      width={4}
+                      height={4}
+                      className="text-gray-200"
+                      fill="currentColor"
+                    />
+                  </pattern>
+                </defs>
+                <rect
+                  width={404}
+                  height={384}
+                  fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+                />
+              </svg>
+            </div>
           </div>
-        </div>
-        <div className="relative px-4 sm:px-6 lg:px-8">
-          {blog?.title ? (
+          <div className="relative px-4 sm:px-6 lg:px-8">
             <div className="max-w-7xl mx-auto">
               <h1>
                 <span className="mt-4 block text-2xl text-center leading-8 font-bold tracking-tight text-gray-900 sm:text-4xl font-heading">
@@ -299,11 +299,11 @@ const Blog = ({ blog }) => {
                 <BlogFooter />
               </div>
             </div>
-          ) : (
-            <Error statusCode={404} />
-          )}
+          </div>
         </div>
-      </div>
+      ) : (
+        <Error statusCode={404} />
+      )}
     </div>
   );
 };
