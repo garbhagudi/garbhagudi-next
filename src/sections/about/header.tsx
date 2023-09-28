1c4c29c97148901091fd64a9c360b168225afa47
Inqonsecuential <theavinashmishra13@gmail.com>
2023-09-28 15:23:01 +0530

    added gg-way page

--- src/sections/about/header.tsx
+++ src/sections/about/header.tsx
@@ -1,42 +1,69 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
-    <div className='relative bg-transparent overflow-hidden'>
-      <div className='max-w-7xl mx-auto'>
-        <div className='relative z-10 pb-8 bg-transparent sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32'>
-          <svg
-            className='hidden xl:block absolute right-0 inset-y-0 h-full w-56 mr-4 text-white transform translate-x-1/2'
-            fill='currentColor'
-            viewBox='0 0 100 100'
-            preserveAspectRatio='none'
-            aria-hidden='true'
-          >
-            <polygon points='50,0 100,0 50,100 0,100' />
-          </svg>
-          <div className='relative pt-6 px-4 sm:px-6 lg:px-8'></div>
+    <div>
+      <div className='relative bg-transparent overflow-hidden'>
+        <div className='max-w-7xl mx-auto'>
+          <div className='relative z-10 pb-8 bg-transparent sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32'>
+            <svg
+              className='hidden xl:block absolute right-0 inset-y-0 h-full w-56 mr-4 text-white transform translate-x-1/2'
+              fill='currentColor'
+              viewBox='0 0 100 100'
+              preserveAspectRatio='none'
+              aria-hidden='true'
+            >
+              <polygon points='50,0 100,0 50,100 0,100' />
+            </svg>
+            <div className='relative pt-6 px-4 sm:px-6 lg:px-8'></div>
 
-          <main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
-            <div className='sm:text-center lg:text-left'>
-              <h1 className='text-4xl tracking-tight sm:text-5xl md:text-6xl font-content font-bold text-brandliteGray'>
-                <span className='block xl:inline '>About GarbhaGudi</span>
-              </h1>
-              <p className='mt-3 text-base text-brandDark sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-content overflow-visible z-10'>
-                GarbhaGudi is a chain of new generation infertility treatment
-                hospitals equipped with state-of-the-art infrastructure and
-                cutting-edge technology to address this ever-increasing problem
-                of infertility. It was founded by a team of committed
-                entrepreneurs and healthcare specialists, led by Dr. Asha S
-                Vijay, who is a renowned gynecologist and fertility specialist.
-              </p>
-            </div>
-          </main>
+            <main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
+              <div className='sm:text-center lg:text-left'>
+                <h1 className='text-4xl tracking-tight sm:text-5xl md:text-6xl font-content font-bold text-brandliteGray'>
+                  <span className='block xl:inline '>About GarbhaGudi</span>
+                </h1>
+                <p className='mt-3 text-base text-brandDark sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 font-content overflow-visible z-10'>
+                  GarbhaGudi is a chain of new generation infertility treatment
+                  hospitals equipped with state-of-the-art infrastructure and
+                  cutting-edge technology to address this ever-increasing
+                  problem of infertility. It was founded by a team of committed
+                  entrepreneurs and healthcare specialists, led by Dr. Asha S
+                  Vijay, who is a renowned gynecologist and fertility
+                  specialist.
+                </p>
+              </div>
+            </main>
+          </div>
+        </div>
+        <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 '>
+          <img
+            className='h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full '
+            src='https://res.cloudinary.com/garbhagudiivf/image/upload/q_auto:eco/v1643461022/Banner/About_us_xg1b1l.webp'
+            alt='GarbhaGudi IVF Centre'
+          />
         </div>
       </div>
-      <div className='lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 '>
-        <img
-          className='h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full '
-          src='https://res.cloudinary.com/garbhagudiivf/image/upload/q_auto:eco/v1643461022/Banner/About_us_xg1b1l.webp'
-          alt='GarbhaGudi IVF Centre'
-        />
+      <div className='bg-purple-100 px-4 md:px-6 lg:px-8'>
+        <div className='max-w-7xl mx-auto py-4 flex items-center lg:justify-between flex-col md:flex-row'>
+          <div className='py-10 w-full md:w-3/4 text-center md:text-left'>
+            <div className='text-2xl md:text-3xl font-heading tracking-tight font-bold text-brandPink'>
+              Discover the Garbhagudi Way
+            </div>
+            <div className='text-base md:text-lg font-content mt-3 font-semibold'>
+              Discover our values and how we operate as an organization. Gain
+              insight into our guiding principles, from Couple's Delight to
+              Holistic Approach.
+            </div>
+          </div>
+          <div className='mx-auto mb-4 md:mb-0'>
+            <Link
+              href={'/about/garbhagudi-way'}
+              className='px-4 py-3 font-content font-semibold bg-brandPink text-white text-lg rounded-md hover:bg-brandPink3 transition-all duration-200'
+            >
+              Know More
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/sections/tools/gg-way.tsx
+++ src/sections/tools/gg-way.tsx
@@ -3,41 +3,40 @@ import React from 'react';
 const GarbhaGudiWay = () => {
   return (
     <div>
-      <div className='bg-purple-100'>
-        <h1 className='py-8 text-3xl lg:text-4xl font-heading text-center font-bold'>
-          Garbhagudi Way
+      <div className='bg-purple-50'>
+        <h1 className='py-8 lg:py-10 text-3xl lg:text-4xl font-lexend text-center font-bold'>
+          GarbhaGudi Way
         </h1>
-        <p className='max-w-4xl mx-auto pb-8 text-lg font-content text-center'>
+        <p className='max-w-4xl mx-auto pb-8 text-lg font-lexend text-center'>
           Whether you are an individual contributor or the manager of a large
-          team, you are a GarbhaGudi leader - A GarbhaGudian. All GarbhaGudians
-          work with 11 guiding principles.
+          team, you are a GarbhaGudi leader - A GarbhaGudian.{' '}
+          <span className='font-extrabold text-brandPink4'>
+            All GarbhaGudians work with 11 guiding principles.
+          </span>
         </p>
       </div>
-      <div className='flex flex-wrap gap-5 items-center justify-center bg-purple-100 py-10 px-3 sm:px-0'>
+      <div className='flex flex-wrap gap-5 items-center justify-center bg-purple-50 py-10 px-3 sm:px-0'>
         {ggway.map((items) => (
           <div
-            className='group h-72 w-72 md:h-96 md:w-96 [perspective:1000px]'
+            className='group w-full max-w-sm aspect-square [perspective:1000px]'
             key={items.id}
           >
             <div className='relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]'>
               <div className='relative w-full h-full inset-0'>
-                <div className='absolute w-full h-full bg-black/40 rounded-lg'>
-                  <div className='flex h-full px-2 items-center text-white/90 text-xl md:text-3xl text-center font-heading justify-center group-hover:text-transparent transition-all duration-500'>
+                <div className='absolute w-full h-full bg-black/70 rounded-lg'>
+                  <div className='flex h-full px-2 items-center font-bold text-white text-2xl md:text-3xl text-center font-lexend justify-center group-hover:text-transparent transition-all duration-500'>
                     {items.title}
                   </div>
                 </div>
                 <img
-                  className='w-full h-full rounded-xl object-cover shadow-xl shadow-black/40'
+                  className='w-full h-full rounded-xl object-cover shadow-xl shadow-black/50'
                   src={items.bgImage}
                   alt='Image'
                 />
               </div>
-              <div className='absolute inset-0 h-full w-full rounded-xl bg-brandDark/70 px-3 py-1 md:px-12 text-center text-slate-200 [transform:rotateY(180deg)] [backface-visibility:hidden]'>
-                <div className='flex min-h-full flex-col items-center justify-center font-content text-sm md:text-xl'>
+              <div className='absolute inset-0 h-full w-full rounded-xl bg-brandDark/70 px-3 py-1 md:px-12 text-center text-white [transform:rotateY(180deg)] [backface-visibility:hidden]'>
+                <div className='flex min-h-full flex-col items-center justify-center font-lexend text-lg md:text-xl'>
                   {items.way}
-                  {/* <button className="mt-10 px-3 py-2 border-2 border-white rounded-lg hover:bg-brandPink hover:text-white text-base md:text-lg">
-                    Watch Video
-                  </button> */}
                 </div>
               </div>
             </div>
@@ -54,67 +53,165 @@ const ggway = [
   {
     id: 1,
     title: "Couple's Delight",
-    way: 'Every couple who walks in, will be treated like a King and Queen. Every couple will feel that they are in the right place to complete their family. Every couple who walks in will walk out with a smile. We believe that a satisfied client is our best brand evangelist.',
-    bgImage: 'https://unsplash.it/640/425',
+    way: (
+      <div>
+        Every couple who walks in, will be treated like a King and Queen. Every
+        couple will feel that they are in the right place to complete their
+        family. Every couple who walks in will walk out with a smile.{' '}
+        <span className='font-extrabold text-brandPink4'>
+          We believe that a satisfied client is our best brand evangelist.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/640',
   },
   {
     id: 2,
     title: 'Ownership',
-    way: "Leaders are owners. They think long term and don't sacrifice long term values for short term results. They always say, I am ready to take responsibility.",
-    bgImage: 'https://unsplash.it/640/640',
+    way: (
+      <div>
+        Leaders are owners. They think long term and don't sacrifice long term
+        values for short term results. They always say,{' '}
+        <span className='font-extrabold text-brandPink4'>
+          I am ready to take responsibility.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/641',
   },
   {
     id: 3,
     title: 'Passion for Excellance',
-    way: 'Leaders strive for excellence in everything they do to deliver higher standards. Leaders find solutions in all situations that stare at them.',
-    bgImage: 'https://unsplash.it/640/645',
+    way: (
+      <div>
+        Leaders strive for excellence in everything they do to deliver higher
+        standards.{' '}
+        <span className='font-extrabold text-brandPink4'>
+          Leaders find solutions in all situations that stare at them.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/642',
   },
   {
     id: 4,
     title: 'Growth',
-    way: 'Growth is at the center of our personal, professional and organizational aspirations. We dare to think big and aspire to achieve them all.',
-    bgImage: 'https://unsplash.it/640/630',
+    way: (
+      <div>
+        Growth is at the center of our personal, professional and organizational
+        aspirations. We{' '}
+        <span className='font-extrabold text-brandPink4'>
+          dare to think big and aspire to achieve them all.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/639',
   },
   {
     id: 5,
     title: 'Culture of Genuinity',
-    way: 'We as leaders always adhere to GarbhaGudi culture of strong ethical values, transparency and humane touch.',
-    bgImage: 'https://unsplash.it/630/630',
+    way: (
+      <div>
+        We as leaders always adhere to{' '}
+        <span className='font-extrabold text-brandPink4'>
+          GarbhaGudi culture of strong ethical values, transparency and humane
+          touch.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/643',
   },
   {
     id: 6,
     title: 'Standpoint',
-    way: 'We as leaders believe in financial loyalty to the organization and service loyalty to the couple.',
-    bgImage: 'https://unsplash.it/635/630',
+    way: (
+      <div>
+        We as leaders believe in{' '}
+        <span className='font-extrabold text-brandPink4'>
+          financial loyalty to the organization and service loyalty to the
+          couple.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/638',
   },
   {
     id: 7,
     title: 'Team Work',
-    way: "Together we work as a team to accomplish our common vision. We never say, that's not my job. Instead, we assume unconditional responsibility to fix an issue even if it is beyond the scope of one's role.",
-    bgImage: 'https://unsplash.it/630/650',
+    way: (
+      <div>
+        Together we work as a team to accomplish our common vision. We never
+        say, that's not my job. Instead, we assume{' '}
+        <span className='font-extrabold text-brandPink4'>
+          unconditional responsibility to fix an issue even if it is beyond the
+          scope of one's role.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/637',
   },
   {
     id: 8,
     title: 'Hire and develop to raise the bar',
-    way: 'Leaders raise the performance bar with every hire and promotion. They are willing to recognise exceptional talent, coach them and guide them throughout',
-    bgImage: 'https://unsplash.it/640/660',
+    way: (
+      <div>
+        Leaders raise the performance bar with every hire and promotion. They
+        are willing to{' '}
+        <span className='font-extrabold text-brandPink4'>
+          recognise exceptional talent, coach them and guide them throughout
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/644',
   },
   {
     id: 9,
     title: 'Partners in Progress',
-    way: 'As Leaders we respect every individual, including Suppliers, Vendors & Service Providers because we know they are our Brand Ambassadors.',
-    bgImage: 'https://unsplash.it/620/650',
+    way: (
+      <div>
+        As Leaders{' '}
+        <span className='font-extrabold text-brandPink4'>
+          we respect every individual, including Suppliers, Vendors & Service
+          Providers
+        </span>{' '}
+        because we know they are our{' '}
+        <span className='font-extrabold text-brandPink4'>
+          Brand Ambassadors
+        </span>
+        .
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/636',
   },
   {
     id: 10,
     title: 'Deliver Results',
-    way: 'At the end of the day, results speak louder than words. Leaders for delivering results by going the extra mile.',
-    bgImage: 'https://unsplash.it/625/625',
+    way: (
+      <div>
+        At the end of the day,{' '}
+        <span className='font-extrabold text-brandPink4'>
+          results speak louder than words.
+        </span>{' '}
+        Leaders for delivering{' '}
+        <span className='font-extrabold text-brandPink4'>
+          results by going the extra mile.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/635',
   },
   {
     id: 11,
     title: 'Holistic Approach',
-    way: 'Leaders will not be one role wonders. They will always be holistic growing in all dimensions - Healthy, Wealthy, Loving, Blissful and Peaceful.',
-    bgImage: 'https://unsplash.it/650/650',
+    way: (
+      <div>
+        Leaders will not be one role wonders. They will{' '}
+        <span className='font-extrabold text-brandPink4'>
+          always be holistic growing in all dimensions - Healthy, Wealthy,
+          Loving, Blissful and Peaceful.
+        </span>
+      </div>
+    ),
+    bgImage: 'https://unsplash.it/640/646',
   },
 ];
