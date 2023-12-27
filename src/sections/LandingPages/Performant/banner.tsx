a42ec1aef151b0d97363a8faf8ddee2c6881d1d8
Inqonsecuential <theavinashmishra13@gmail.com>
2023-12-27 16:56:24 +0530

    Refactor landing page components and update banner design

--- src/sections/LandingPages/Performant/banner.tsx
+++ src/sections/LandingPages/Performant/banner.tsx
@@ -5,21 +5,19 @@ import Link from 'next/link';
 const Banner = () => {
   return (
     <div>
-      <div className='grid grid-cols-1 lg:grid-cols-3 bg-[#cad4ea]'>
-        <div className='order-0 relative font-lexend'>
-          <div className='flex items-center justify-center w-full h-full flex-col text-center py-16 lg:py-0'>
+      <div className='grid grid-cols-1 lg:grid-cols-3 '>
+        <div className='order-0 relative'>
+          <div className='flex items-left justify-center w-full h-full flex-col text-center py-16 lg:py-0 bg-[url(https://res.cloudinary.com/garbhagudi/image/upload/v1702778382/WhatsApp_Image_2023-12-16_at_16.06.34_b3904a5d_lbfffv.webp)] bg-cover bg-no-repeat'>
             <div className='text-4xl lg:text-5xl font-heading font-extrabold text-center pb-4'>
-              <div className='text-gg-500 uppercase'>Paripoorna</div>
+              <div className='text-[#e92e64] uppercase'>Paripoorna</div>
             </div>
             <div className='text-xl font-bold pb-2'>
               It&apos;s the season of JOY of being COMPLETE
             </div>
-            <div className='text-xl py-2 pb-4'>
-              Under Paripoorna, get a flat discount of{' '}
-              <span className='text-[#064a63] font-bold underline'>
-                ₹ 90,000/-
-              </span>{' '}
-              on IVF treatment program*
+            <div className='text-xl py-2 font-bold font-content pb-4 px-3'>
+              Get a flat discount of{' '}
+              <span className='text-[#064a63] underline'>₹90,000/-</span> on IVF
+              treatment program*
             </div>
           </div>
           <div className='font-content -mt-5 text-xs font-medium hidden lg:block'>
@@ -32,7 +30,7 @@ const Banner = () => {
           </div>
           <Image
             src={
-              'https://res.cloudinary.com/garbhagudi/image/upload/v1700224143/garbhagudi-ivf/Paripoorna/Web_Banner_New_Artboard_copy_5_or5khw.webp'
+              'https://res.cloudinary.com/garbhagudi/image/upload/v1702632912/WhatsApp_Image_2023-12-15_at_15.02.53_9634526f_pvmr6o.webp'
             }
             width={1920}
             height={1080}
@@ -47,7 +45,7 @@ const Banner = () => {
           </div>
         </div>
       </div>
-      <div className='max-w-7xl mx-auto sm:mt-10 font-lexend'>
+      <div className='max-w-7xl mx-auto sm:mt-10 '>
         <div className='text-base md:text-lg text-center mb-4 px-3 py-2 bg-gray-300 max-w-lg font-semibold mx-auto sm:rounded-lg'>
           To Book Appointment call{' '}
           <a
@@ -59,7 +57,7 @@ const Banner = () => {
         </div>
         <div className='mx-auto text-center'>
           <button className='px-3 py-2 text-center mx-auto block sm:inline-block bg-brandPink hover:bg-brandPurpleDark transition duration-200 ease-in-out text-white font-semibold rounded-lg'>
-            <Link href='#leadForm' className='scroll-smooth'>
+            <Link href='#leadForm' className='scroll-smooth font-sans'>
               Book an Appointment
             </Link>
           </button>
