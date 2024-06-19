97b8363ffc630c186f957bb02ab2b58893e81ea6
dbugging404 <theavinashmishra13@gmail.com>
2024-06-19 12:49:26 +0530

    chore: Optimize font loading in RootLayout component

---
+++ src/components/layout.tsx
@@ -0,0 +1,43 @@
+import { Lexend, Nunito, B612, Benne } from 'next/font/google';
+
+const lexend = Lexend({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lexend',
+});
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-nunito',
+});
+
+const b612 = B612({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-b612',
+  weight: ['400', '700'],
+});
+
+const benne = Benne({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-benne',
+  weight: ['400'],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <main
+        className={`${lexend.variable} ${nunito.variable} ${b612.variable} ${benne.variable}`}
+      >
+        {children}
+      </main>
+    </>
+  );
+}
