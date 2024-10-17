86264d7ca4f6400ede69d0900d3b9609b37e3371
Avinash <avinashmishra13@hotmail.com>
2024-10-17 21:28:47 +0530

    fix: update iframe source and improve form layout in contact page (#1038)

--- src/sections/LandingPages/Performant/form.tsx
+++ src/sections/LandingPages/Performant/form.tsx
@@ -4,7 +4,7 @@ const Form = () => {
   return (
     <div className='overflow-hidden rounded-lg'>
       <iframe
-        src='/contact2.html'
+        src='/contact9.html'
         className='mx-auto h-screen max-h-[460px] w-screen max-w-sm rounded-lg bg-transparent dark:bg-transparent'
         loading='lazy'
       >
