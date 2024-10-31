4a2efa2de116f5599a860e92fc1ee23014bcb62c
Avinash Mishra <theavinashmishra13@gmail.com>
2024-10-31 09:07:01 +0530

    #1055 - fix: update contact phone numbers and support links and Dailyhunt pixelcode render fix

--- src/sections/gg-care/hero.tsx
+++ src/sections/gg-care/hero.tsx
@@ -17,7 +17,7 @@ const supportLinks = [
       'Reach out to us with your questions, concerns, or challenges. We’ll be happy to help you at any time, and we’re always trying to make things easier for you!',
     icon: HiPhone,
     ctaAction: 'Call Now',
-    ctaLink: 'tel:+919071234003',
+    ctaLink: 'tel:+919480948005',
   },
   {
     name: 'Book an Appointment',
