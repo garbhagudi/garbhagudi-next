9df87411031c1104405aaa85f0f9c83ddf09838c
Avinash Mishra <theavinashmishra13@gmail.com>
2024-11-04 20:22:04 +0530

    build fix

--- src/styles/theme-provider.tsx
+++ src/styles/theme-provider.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
-import type { ThemeProviderProps } from 'next-themes/dist/types';
 
-const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
+const ThemeProvider = ({ children, ...props }) => {
   return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
 };
 
