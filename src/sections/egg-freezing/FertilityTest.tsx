53c5deb6110344ded349f8e22dc8b2d3e2fc7c9e
saravanan <saravanan10722@gmail.com>
2024-11-21 16:53:35 +0530

    image added

--- src/sections/egg-freezing/FertilityTest.tsx
+++ src/sections/egg-freezing/FertilityTest.tsx
@@ -4,27 +4,27 @@ import Link from 'next/link';
 import BookAnAppointment from './bookAnAppointment';
 
 export default function FertilityTest() {
-  const renderData = [
-    {
-      title: 'Select a convenient time for your blood sample collection.',
-      subTitle:
-        'When booking, choose a time that works for you for the easy-to-do Fertility Test blood test.',
-    },
-    {
-      title: 'Our diagnostic partner will collect your blood sample.',
-      subTitle:
-        'A friendly technician will visit your home for blood collection. Drink water and fast for 12 hours, just like a regular test.',
-    },
-    {
-      title: 'View your fertility analysis.',
-      subTitle: 'View your results online in 48 hours.',
-    },
-    {
-      title: 'Consult with a fertility coach.',
-      subTitle:
-        'Post-results, our care team will help you navigate what’s next—from getting an ultrasound to speaking to a fertility specialist.',
-    },
-  ];
+  // const renderData = [
+  //   {
+  //     title: 'Select a convenient time for your blood sample collection.',
+  //     subTitle:
+  //       'When booking, choose a time that works for you for the easy-to-do Fertility Test blood test.',
+  //   },
+  //   {
+  //     title: 'Our diagnostic partner will collect your blood sample.',
+  //     subTitle:
+  //       'A friendly technician will visit your home for blood collection. Drink water and fast for 12 hours, just like a regular test.',
+  //   },
+  //   {
+  //     title: 'View your fertility analysis.',
+  //     subTitle: 'View your results online in 48 hours.',
+  //   },
+  //   {
+  //     title: 'Consult with a fertility coach.',
+  //     subTitle:
+  //       'Post-results, our care team will help you navigate what’s next—from getting an ultrasound to speaking to a fertility specialist.',
+  //   },
+  // ];
 
   const [isOpen, setIsOpen] = useState(false);
   const [bookNow, setBookNow] = useState(false);
@@ -65,59 +65,48 @@ export default function FertilityTest() {
     },
   ];
   return (
-    <div className='bg-[#B03146] pb-10 pt-10 md:pb-20'>
-      <div className='flex items-center justify-center pb-10 font-nunito-Sans text-2xl font-extrabold text-white md:text-4xl'>
-        Fertility Test - 4 Simple Steps
+    <div className='pb-10 pt-5 md:pb-0'>
+      <div className='flex items-center justify-center pb-10 font-nunito-Sans text-2xl font-extrabold text-black md:text-4xl'>
+        Fertility Test
       </div>
-      <div className='mx-auto flex w-10/12 flex-col justify-between gap-10 md:flex-row'>
-        <div className='relative h-[400px] md:h-[350px] md:w-full'>
-          <Image
-            src={
-              'https://res.cloudinary.com/garbhagudiivf/image/upload/v1731567197/eggFreezing/h55ajjflanab267q8ssm.png'
-            }
-            width={350}
-            height={350}
-            alt='Image 1'
-            loading='lazy'
-            className='absolute left-7 top-0 h-[400px] w-[300px] md:left-0 md:h-[500px] md:w-[450px]'
-          />
-
-          {/* Second Image (middle layer) */}
-          <Image
-            src={
-              'https://res.cloudinary.com/garbhagudiivf/image/upload/v1731567197/eggFreezing/h2rbj9cognzqtpzelove.png'
-            }
-            width={350}
-            height={350}
-            alt='Image 2'
-            loading='lazy'
-            className='absolute left-10 top-3 h-[400px] w-[300px] md:left-3 md:h-[500px] md:w-[450px]'
-          />
-        </div>
-
-        <div className='mx-auto mt-10 w-11/12 font-nunito-Sans md:w-full md:text-start'>
-          {renderData.map((ele, index) => (
-            <div key={index} className='py-2 text-white'>
-              <div className='py-2 text-lg font-semibold leading-5 md:py-0 md:text-xl md:leading-6'>
-                {index + 1}. {ele.title}
-              </div>
-              <div className='py-1 pl-5 text-lg font-medium opacity-80'>
-                {ele.subTitle}
-              </div>
-              {renderData.length === index + 1 && (
-                <div className='mt-7 w-2/3 lg:mt-14 lg:w-1/3'>
-                  <span
-                    className='flex cursor-pointer items-center justify-center scroll-smooth rounded bg-white px-4 py-2 font-content font-semibold text-[#D9576C] shadow hover:text-gg-400 focus:outline-none focus:ring active:text-rose-500 dark:bg-gg-500 dark:text-white dark:hover:bg-gg-400 sm:w-auto'
-                    onClick={openModal}
-                  >
-                    Explore Packages
-                  </span>
-                </div>
-              )}
-            </div>
-          ))}
+      <div className='flex flex-col justify-center gap-5 px-5 md:px-10 lg:flex-row lg:gap-0'>
+        <Image
+          src={
+            'https://res.cloudinary.com/garbhagudiivf/image/upload/v1732186483/eggFreezing/bj8btmanhoukexoviepg.png'
+          }
+          alt={''}
+          width={100}
+          height={100}
+          className='h-[400px] w-full rounded-md'
+        />
+        <div className='px-5 lg:pl-5'>
+          <p className='text-2xl font-semibold'>
+            Understanding AMH – Anti Mullerian Hormone
+          </p>
+          <ul className='list-inside list-disc py-2 font-nunito-Sans text-lg text-[#6C6C6C]'>
+            <li className='custom-list-item text-wrap'>
+              A hormone produced by the ovaries in females and the testicles in
+              males that helps develop reproductive organs. AMH levels can be
+              measured in the blood to help diagnose conditions and make
+              decisions about fertility treatments.
+            </li>
+            <li className='custom-list-item text-wrap'>
+              AMH levels indicate the number of eggs remaining in the ovaries,
+              also known as ovarian reserve. AMH levels decrease with age, and
+              can be used to estimate how long a woman has left to conceive.
+            </li>
+          </ul>
+          <div className='mt-7 w-2/3 md:w-1/3 lg:mt-10'>
+            <span
+              className='flex cursor-pointer items-center justify-center scroll-smooth rounded bg-[#D9576C] px-4 py-2 font-content font-semibold text-white shadow hover:text-gg-400 focus:outline-none focus:ring dark:bg-gg-500 dark:text-white dark:hover:bg-gg-400 sm:w-auto'
+              onClick={openModal}
+            >
+              Explore Packages
+            </span>
+          </div>
         </div>
       </div>
+
       {isOpen && (
         <div
           className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'
@@ -186,4 +175,56 @@ export default function FertilityTest() {
       )}
     </div>
   );
+}
+
+{
+  /* <div className='mx-auto flex w-10/12 flex-col justify-between gap-10 md:flex-row'>
+<div className='relative h-[400px] md:h-[350px] md:w-full'>
+  <Image
+    src={
+      'https://res.cloudinary.com/garbhagudiivf/image/upload/v1731567197/eggFreezing/h55ajjflanab267q8ssm.png'
+    }
+    width={350}
+    height={350}
+    alt='Image 1'
+    loading='lazy'
+    className='absolute left-7 top-0 h-[400px] w-[300px] md:left-0 md:h-[500px] md:w-[450px]'
+  />
+
+
+  <Image
+    src={
+      'https://res.cloudinary.com/garbhagudiivf/image/upload/v1731567197/eggFreezing/h2rbj9cognzqtpzelove.png'
+    }
+    width={350}
+    height={350}
+    alt='Image 2'
+    loading='lazy'
+    className='absolute left-10 top-3 h-[400px] w-[300px] md:left-3 md:h-[500px] md:w-[450px]'
+  />
+</div>
+
+<div className='mx-auto mt-10 w-11/12 font-nunito-Sans md:w-full md:text-start'>
+  {renderData.map((ele, index) => (
+    <div key={index} className='py-2 text-white'>
+      <div className='py-2 text-lg font-semibold leading-5 md:py-0 md:text-xl md:leading-6'>
+        {index + 1}. {ele.title}
+      </div>
+      <div className='py-1 pl-5 text-lg font-medium opacity-80'>
+        {ele.subTitle}
+      </div>
+      {renderData.length === index + 1 && (
+        <div className='mt-7 w-2/3 lg:mt-14 lg:w-1/3'>
+          <span
+            className='flex cursor-pointer items-center justify-center scroll-smooth rounded bg-white px-4 py-2 font-content font-semibold text-[#D9576C] shadow hover:text-gg-400 focus:outline-none focus:ring active:text-rose-500 dark:bg-gg-500 dark:text-white dark:hover:bg-gg-400 sm:w-auto'
+            onClick={openModal}
+          >
+            Explore Packages
+          </span>
+        </div>
+      )}
+    </div>
+  ))}
+</div>
+</div> */
 }
