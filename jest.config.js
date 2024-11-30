14526e5456e7165699f30e2296e2715390d19b33
Avinash <theavinashmishra13@gmail.com>
2024-11-30 09:59:29 +0530

    initial setup for tests

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  testTimeout: 13000,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: { '^@/(.*)$': '<rootDir>/src/$1' },
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'clover'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '/coverage/',
+    '/public/',
+    '/.storybook/',
+    '/.vscode/',
+    '/.github/',
+    '/.husky/',
+    '/.vscode/',
+    '/.git/',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
+
+module.exports = customJestConfig(createJestConfig);
