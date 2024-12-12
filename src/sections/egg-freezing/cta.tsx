9d7324bf79e66273614c7a164c4366496b2356c6
tamil-witarist <tamil@witarist.com>
2024-12-12 14:02:55 +0530

    LP for egg-freezing page updated

--- src/sections/egg-freezing/cta.tsx
+++ src/sections/egg-freezing/cta.tsx
@@ -32,7 +32,7 @@ const CTA: React.FC = () => {
           className='fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50'
           onClick={handleClose}
         >
-         <Form />
+          <Form />
         </div>
       )}
     </div>
