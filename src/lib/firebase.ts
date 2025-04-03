6c4816cced24d1e608ceb5c16cc10ff100ef0db4
ashish-witarist <ashish@witarist.com>
2025-04-03 17:12:45 +0530

    fixed build issue

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,13 @@
-
-import { initializeApp } from "firebase/app";
-import { getMessaging, isSupported } from "firebase/messaging";
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
 const firebaseConfig = {
-  apiKey: "AIzaSyCW5sakVoJ6eLv5nWeRlgsIRN3aG8JQ-VM",
-  authDomain: "garbhagudi-test-ad096.firebaseapp.com",
-  projectId: "garbhagudi-test-ad096",
-  storageBucket: "garbhagudi-test-ad096.firebasestorage.app",
-  messagingSenderId: "1094258951345",
-  appId: "1:1094258951345:web:fb84a21b75faa9f60ace90",
-  measurementId: "G-EJTJ61STHD"
+  apiKey: 'AIzaSyCW5sakVoJ6eLv5nWeRlgsIRN3aG8JQ-VM',
+  authDomain: 'garbhagudi-test-ad096.firebaseapp.com',
+  projectId: 'garbhagudi-test-ad096',
+  storageBucket: 'garbhagudi-test-ad096.firebasestorage.app',
+  messagingSenderId: '1094258951345',
+  appId: '1:1094258951345:web:fb84a21b75faa9f60ace90',
+  measurementId: 'G-EJTJ61STHD',
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -16,7 +15,7 @@ export const app = initializeApp(firebaseConfig);
 let messaging = null;
 
 // Ensure it's only accessed in the browser
-if (typeof window !== "undefined") {
+if (typeof window !== 'undefined') {
   messaging = getMessaging(app);
 }
 
