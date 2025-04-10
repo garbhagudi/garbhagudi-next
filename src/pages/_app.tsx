4aa70b7d342212aea733fc0417e4063e05c74174
ashish-witarist <ashish@witarist.com>
2025-04-10 21:42:55 +0530

    added script tag and iframe in _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -119,25 +119,7 @@ function MyApp({ Component, pageProps }) {
       /> */}
 
       {/* GTM Script in Head */}
-      <Script id='gtm-head' strategy='afterInteractive'>
-        {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5T77DVZ');
-        `}
-      </Script>
 
-      {/* GTM noscript fallback */}
-      <noscript>
-        <iframe
-          src='https://www.googletagmanager.com/ns.html?id=GTM-5T77DVZ'
-          height='0'
-          width='0'
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
       <ThemeProvider attribute='class' defaultTheme='light'>
         {loading ? (
           <Loading />
