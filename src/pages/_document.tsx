7f3d1b8d14d4019b4f188b40c8d03dc760db38d3
ashish-witarist <ashish@witarist.com>
2025-04-10 21:58:20 +0530

    fix it

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,11 +8,13 @@ class MyDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
+              <!-- Google Tag Manager -->
               (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','GTM-5T77DVZ');
+              <!-- End Google Tag Manager -->
             `,
             }}
           />
@@ -21,8 +23,10 @@ class MyDocument extends Document {
           <noscript
             dangerouslySetInnerHTML={{
               __html: `
+              <!-- Google Tag Manager (noscript) -->
               <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T77DVZ"
               height="0" width="0" style="display:none;visibility:hidden"></iframe>
+              <!-- End Google Tag Manager (noscript) -->
             `,
             }}
           />
