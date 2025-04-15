16def5b10bde44482f2d19cd4c10cc1b0b1a3c43
ashish-witarist <ashish@witarist.com>
2025-04-15 13:51:46 +0530

    broken link and og title and description added

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -104,7 +104,9 @@ const Blog = ({ blog }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>{title}</title>
         <meta name='title' content={title} />
+        <meta property='og:title' content={title} />
         <meta name='description' content={description} />
+        <meta property='og:description' content={description} />
         <meta name='keywords' content={keywords} />
 
         {/* Ld+JSON Data */}
