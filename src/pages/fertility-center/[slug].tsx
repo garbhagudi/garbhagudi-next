8498a5dbe05c6202a5682cf0e1625d1f3314a663
ashish-witarist <ashish@witarist.com>
2025-04-23 12:51:15 +0530

    fixed hyd issue

--- src/pages/fertility-center/[slug].tsx
+++ src/pages/fertility-center/[slug].tsx
@@ -252,7 +252,13 @@ const Blog = ({ article }: BlogProps) => {
                   ul: ({ children }) => <ul className='list-disc pl-6'>{children}</ul>,
                   ol: ({ children }) => <ol className='list-decimal pl-6'>{children}</ol>,
                   li: ({ children }) => <li>{children}</li>,
-                  // you can also add p, h2, etc., if needed
+                  p: ({ children }) => <p className='mb-2'>{children}</p>,
+                  bold: ({ children }) => <strong>{children}</strong>,
+                  a: ({ children, href }) => (
+                    <a href={href} className='text-blue-600 underline'>
+                      {children}
+                    </a>
+                  ),
                 }}
               />
             </div>
