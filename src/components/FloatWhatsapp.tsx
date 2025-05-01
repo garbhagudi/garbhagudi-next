f22af1ec91e2064055962ac676be99ad1ce46447
ashish-witarist <ashish@witarist.com>
2025-05-01 18:17:06 +0530

    changed whatsapp number

--- src/components/FloatWhatsapp.tsx
+++ src/components/FloatWhatsapp.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const FloatWhatsApp = () => {
   return (
     <div className='fixed bottom-5 right-5 flex cursor-pointer items-center justify-center rounded-full bg-green-600 px-2 py-2 font-content font-bold text-white'>
-      <Link href='https://wa.me/918951813344'>
+      <Link href='https://wa.me/919108910832'>
         <FaWhatsapp className='h-5 w-5 text-white md:h-8 md:w-8' />
       </Link>
     </div>
