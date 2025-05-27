01ecc43895b0983d0f80e4c46ffcf3f67e19f62e
ashish-witarist <ashish@witarist.com>
2025-05-27 13:23:34 +0530

    add sitemap manually

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,17 +4,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL,
   generateIndexSitemap: false,
   generateRobotsTxt: false,
-  sitemapSize: 5000,
-  changefreq: 'daily',
-  priority: 0.8,
   exclude: ['/admin/*', '/api/*'],
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/?*'],
-      },
-    ],
-  },
 };
