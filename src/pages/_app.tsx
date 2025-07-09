768d4fa3e2bb1751cc4aa57a3096be7999e78ad1
ashish-witarist <ashish@witarist.com>
2025-07-09 10:41:36 +0530

    removed salesIQ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,7 @@ import FloatWhatsApp from 'components/FloatWhatsapp';
 // Dynamically import components
 const Footer = dynamic(() => import('components/footer/footer'), { ssr: false });
 const Nav = dynamic(() => import('components/header/header'), { ssr: true });
-const Salesiq = dynamic(() => import('components/SalesIQ'), { ssr: false });
+// const Salesiq = dynamic(() => import('components/SalesIQ'), { ssr: false });
 const Loading = dynamic(() => import('components/Loading'), { ssr: true });
 const FloatPhone = dynamic(() => import('components/FloatPhone'), { ssr: false });
 const FloatRequestCallBack = dynamic(() => import('components/FloatRequestCallBack'), {
@@ -37,13 +37,13 @@ function MyApp({ Component, pageProps }) {
 
   const shouldDisplay = !noRenderPaths.includes(router.pathname);
   const [loading, setLoading] = useState(false);
-  const [isMobile, setIsMobile] = useState(false);
-  useEffect(() => {
-    const userAgent = typeof window !== 'undefined' ? navigator.userAgent : '';
-    setIsMobile(/Mobi|Android/i.test(userAgent));
-  }, []);
+  // const [isMobile, setIsMobile] = useState(false);
+  // useEffect(() => {
+  //   const userAgent = typeof window !== 'undefined' ? navigator.userAgent : '';
+  //   setIsMobile(/Mobi|Android/i.test(userAgent));
+  // }, []);
 
-  const showSalesIQ = !(router.pathname === '/contact/enquiry' && isMobile);
+  // const showSalesIQ = !(router.pathname === '/contact/enquiry' && isMobile);
   // const isBlogPage = router.asPath.startsWith('/blogs/');
 
   useEffect(() => {
@@ -120,7 +120,7 @@ function MyApp({ Component, pageProps }) {
           name='robots'
           content='follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:standard'
         />
-        <link rel='preconnect' href='https://salesiq.zoho.com' crossOrigin='anonymous' />
+        {/* <link rel='preconnect' href='https://salesiq.zoho.com' crossOrigin='anonymous' /> */}
         <link rel='preconnect' href='https://media.graphassets.com' />
       </Head>
 
@@ -134,12 +134,12 @@ function MyApp({ Component, pageProps }) {
             {!iuiTreatmentPage && <Footer />}
           </div>
         )}
-        {shouldDisplay && showSalesIQ && (
+        {/* {shouldDisplay && showSalesIQ && (
           <Salesiq
             widgetCode='93210c756ea31b2224df734860e5d813b081008ce54deb21426241464ccb8de2e6558490d76d66086d0b48b1ed4abff0'
             domain='https://salesiq.zoho.com/widget'
           />
-        )}
+        )} */}
       </ThemeProvider>
       <SpeedInsights />
       <FloatRequestCallBack />
