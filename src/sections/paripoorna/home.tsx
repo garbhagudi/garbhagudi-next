8c4f7e80651001ac53acf4a3e6647d3bdc9c90ff
ashish-witarist <ashish@witarist.com>
2025-11-13 13:35:19 +0530

    fixed the paripoorna page ui issue in mobile device

--- src/sections/paripoorna/home.tsx
+++ src/sections/paripoorna/home.tsx
@@ -93,7 +93,7 @@ const Home = () => {
                 IVF Treatments&quot;
               </div>
 
-              <div className='font-lexend text-lg text-gray-800 dark:text-gray-200'>
+              <div className='hidden font-lexend text-lg text-gray-800 dark:text-gray-200 md:block'>
                 Our intention is to serve the maximum number of couples who are missing out on
                 experiencing the joy of parenthood. We understand that many couples cannot afford
                 IVF. Keeping this in mind, GarbhaGudi IVF Centre, in association with GarbhaGnan
@@ -120,8 +120,43 @@ const Home = () => {
                   </div>
                 </div>
               </div>
+              <div className='block font-lexend text-lg text-gray-800 dark:text-gray-200 md:hidden'>
+                Our intention is to serve the maximum number of couples who are missing out on
+                experiencing the joy of parenthood. We understand that many couples cannot afford
+                IVF. Keeping this in mind, GarbhaGudi IVF Centre, in association with GarbhaGnan
+                Foundation, has initiated this program called{' '}
+                <span className='font-bold underline'>PARIPOORNA. </span>
+                <Image
+                  src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1731247437/paripoorna/Paripoorna_English_tfrujt.webp'
+                  alt='pp logo'
+                  width={500}
+                  height={500}
+                  className='mx-auto aspect-square scale-90 lg:scale-105'
+                  priority={true}
+                />
+                <PriceSlash originalPrice={291111} slashedPrice={180000} />
+                This November and December 2025, celebrate the season of parenthood with{' '}
+                <span className='font-bold'>PARIPOORNA. </span> GarbhaGudi IVF Centre is offering a
+                flat <span className='font-bold underline'>₹1,11,111/-</span> off on your IVF cycle
+                cost. Visit the nearest GarbhaGudi IVF Centre to avail the opportunity and take the
+                first step in your journey towards completeness.
+                <div className='mt-10 flex flex-col items-start space-x-0 space-y-3 md:items-center lg:flex-row lg:space-x-5 lg:space-y-0'>
+                  <div
+                    className='min-w-fit cursor-pointer rounded-xl bg-purple-700 px-3 py-2 font-lexend text-white hover:bg-purple-800 dark:bg-purple-800 dark:hover:bg-purple-700'
+                    onClick={open}
+                  >
+                    <div>Book an Appointment</div>
+                  </div>
+                  <div className='min-w-fit cursor-pointer rounded-xl border border-gg-500 px-3 py-2 font-lexend text-gg-500 hover:border-gg-600 hover:bg-gg-500 hover:text-white dark:hover:bg-gg-600'>
+                    <Link href='tel:+919108910832' className='flex items-center gap-2'>
+                      <span>Call us: </span>
+                      <span>9108 9108 32</span>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className='flex flex-col items-center justify-center gap-5 lg:ml-10'>
+            <div className='hidden flex-col items-center justify-center gap-5 md:flex lg:ml-10'>
               <Image
                 src='https://res.cloudinary.com/garbhagudiivf/image/upload/v1731247437/paripoorna/Paripoorna_English_tfrujt.webp'
                 alt='pp logo'
@@ -130,6 +165,17 @@ const Home = () => {
                 className='mx-auto aspect-square scale-90 lg:scale-105'
                 priority={true}
               />
+              <div className='relative aspect-[9/16] w-full max-w-[400px] overflow-hidden rounded-lg'>
+                <LiteYouTubeEmbed
+                  id='TH68dBLfq2I'
+                  title='YouTube Shorts | Example'
+                  poster='maxresdefault'
+                  webp={true}
+                  wrapperClass='lty-playbtn yt-lite'
+                />
+              </div>
+            </div>
+            <div className='mt-5 flex flex-col items-center justify-center gap-5 md:hidden lg:ml-10'>
               <div className='relative aspect-[9/16] w-full max-w-[400px] overflow-hidden rounded-lg'>
                 <LiteYouTubeEmbed
                   id='TH68dBLfq2I'
