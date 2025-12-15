ac89dfafac68d5c1d0ee10ebdcb715f1b2c3d8b9
ashish-witarist <ashish@witarist.com>
2025-12-15 17:51:19 +0530

    fixed text to html ratio

--- src/pages/treatments/[slug].tsx
+++ src/pages/treatments/[slug].tsx
@@ -182,6 +182,8 @@ const Treatment = ({ treatment }) => {
     };
   }
 
+  const metaDescription = treatment?.metaDescription || treatment?.content?.text?.slice(0, 160);
+
   return (
     <div>
       <Head>
@@ -190,10 +192,7 @@ const Treatment = ({ treatment }) => {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>{`${treatment?.metaTitle || treatment?.title}`}</title>
         <meta name='title' content={`${treatment?.metaTitle || treatment?.title}`} />
-        <meta
-          name='description'
-          content={treatment?.metaDescription || treatment?.content?.text.slice(0, 160)}
-        />
+        <meta name='description' content={metaDescription} />
         <meta name='keywords' content={treatment?.metaKeywords} />
         {/* Ld+JSON Data */}
         {treatment && (
@@ -225,7 +224,7 @@ const Treatment = ({ treatment }) => {
         <meta property='og:title' content={`${treatment?.title} | GarbhaGudi IVF Centre`} />
         <meta property='og:site_name' content='GarbhaGudi IVF Centre' />
         <meta property='og:url' content='https://garbhagudi.com' />
-        <meta property='og:description' content={treatment?.content?.text.slice(0, 160)} />
+        <meta property='og:description' content={metaDescription} />
         <meta property='og:type' content='website' />
         <meta property='og:image' content={treatment?.image?.url} />
 
@@ -234,7 +233,7 @@ const Treatment = ({ treatment }) => {
         <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:site' content='@garbhagudiivf' />
         <meta name='twitter:title' content={`${treatment?.title} | GarbhaGudi IVF Centre`} />
-        <meta name='twitter:description' content={treatment?.content?.text.slice(0, 160)} />
+        <meta name='twitter:description' content={metaDescription} />
         <meta name='twitter:image' content={treatment?.image?.url} />
       </Head>
       <BreadCrumbs
@@ -242,100 +241,8 @@ const Treatment = ({ treatment }) => {
         text1='Treatments'
         link2={treatment?.slug}
         text2={treatment?.title}
-        link3={''}
-        text3=''
-        link4={''}
-        text4={''}
       />
-      <div className='relative overflow-hidden bg-white py-16 dark:bg-gray-800'>
-        <div className='hidden lg:absolute lg:inset-y-0 lg:block lg:h-full lg:w-full'>
-          <div className='relative mx-auto h-full max-w-prose text-lg' aria-hidden='true'>
-            <svg
-              className='absolute left-full top-12 translate-x-32 transform'
-              width={404}
-              height={384}
-              fill='none'
-              viewBox='0 0 404 384'
-            >
-              <defs>
-                <pattern
-                  id='74b3fd99-0a6f-4271-bef2-e80eeafdf357'
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className='text-gray-200 dark:text-gray-700'
-                    fill='currentColor'
-                  />
-                </pattern>
-              </defs>
-              <rect width={404} height={384} fill='url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)' />
-            </svg>
-            <svg
-              className='absolute right-full top-1/2 -translate-x-32 -translate-y-1/2 transform'
-              width={404}
-              height={384}
-              fill='none'
-              viewBox='0 0 404 384'
-            >
-              <defs>
-                <pattern
-                  id='f210dbf6-a58d-4871-961e-36d5016a0f49'
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className='text-gray-100 dark:text-gray-700'
-                    fill='currentColor'
-                  />
-                </pattern>
-              </defs>
-              <rect width={404} height={384} fill='url(#f210dbf6-a58d-4871-961e-36d5016a0f49)' />
-            </svg>
-            <svg
-              className='absolute bottom-12 left-full translate-x-32 transform'
-              width={404}
-              height={384}
-              fill='none'
-              viewBox='0 0 404 384'
-            >
-              <defs>
-                <pattern
-                  id='d3eb07ae-5182-43e6-857d-35c643af9034'
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits='userSpaceOnUse'
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className='text-gray-200 dark:text-gray-700'
-                    fill='currentColor'
-                  />
-                </pattern>
-              </defs>
-              <rect width={404} height={384} fill='url(#d3eb07ae-5182-43e6-857d-35c643af9034)' />
-            </svg>
-          </div>
-        </div>
+      <div className='bg-dotted-pattern relative overflow-hidden bg-white py-16 dark:bg-gray-800'>
         <div className='relative px-4 sm:px-6 lg:px-8'>
           <div className='mx-auto max-w-7xl'>
             <h1>
@@ -353,7 +260,7 @@ const Treatment = ({ treatment }) => {
                 priority={true}
               />
             </figure>
-            <div className='text-justify text-gray-800 dark:text-gray-200'>
+            <article className='text-justify text-gray-800 dark:text-gray-200'>
               {treatment?.content?.raw?.children && (
                 <RichText
                   content={treatment.content.raw.children}
@@ -377,16 +284,18 @@ const Treatment = ({ treatment }) => {
                   }}
                 />
               )}
-            </div>
+            </article>
             <div>
               <Share pinmedia={treatment?.image?.url} />
             </div>
           </div>
         </div>
       </div>
-      <div style={{ minHeight: '300px' }}>
-        <FAQs data={treatment?.faq} activeIndex={treatment?.faq[0]?.id} />
-      </div>
+      {treatment?.faq?.length > 0 && (
+        <div style={{ minHeight: '300px' }}>
+          <FAQs data={treatment.faq} activeIndex={treatment.faq[0]?.id} />
+        </div>
+      )}
 
       <Cta />
     </div>
