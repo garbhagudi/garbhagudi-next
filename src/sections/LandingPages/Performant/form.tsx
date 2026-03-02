9e4fdbacbf9a33c357427287f460c75513f0a6d9
sdeveloper123 <sunil@witarist.com>
2026-03-02 12:58:04 +0530

    Fixed issue of thank you page

--- src/sections/LandingPages/Performant/form.tsx
+++ src/sections/LandingPages/Performant/form.tsx
@@ -49,7 +49,6 @@ const Form = () => {
         body: JSON.stringify({ data }),
       });
 
-      const responseData = await response.json();
       if (!response.ok) {
         throw new Error('Network response was not ok');
       }
@@ -66,7 +65,8 @@ const Form = () => {
           },
         });
       }
-      if (response.ok && responseData?.data && responseData.data.length > 0) {
+
+      if (response.ok) {
         router.push('/thank-you.html');
       }
     } catch (err) {
