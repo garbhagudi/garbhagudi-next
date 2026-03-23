9ba6270f5a93fb27c69d764373df321518b4cabf
sdeveloper123 <sunil@witarist.com>
2026-03-23 16:32:10 +0530

    Fixed script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -26,6 +26,22 @@ class MyDocument extends Document {
           `,
             }}
           />
+          <Script
+            id='hotjar'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6673096,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+            }}
+          />
           <noscript
             dangerouslySetInnerHTML={{
               __html: `
