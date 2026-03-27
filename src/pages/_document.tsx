f925a032b59f9d8faf09cf519af41573781296b3
sdeveloper123 <sunil@witarist.com>
2026-03-27 16:08:13 +0530

    Fixed GTM issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,19 +5,27 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en-IN'>
-        <Head />
-        <body>
+        <Head>
+          {/* Google Tag Manager — load early so verification and tags can run */}
           <Script
             id='gtm-script'
-            strategy='lazyOnload'
+            strategy='afterInteractive'
             dangerouslySetInnerHTML={{
               __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5T77DVZ');
-          `,
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NSZH8N9M');
+`,
+            }}
+          />
+        </Head>
+        <body>
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html:
+                '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NSZH8N9M" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
             }}
           />
           <Script
@@ -34,16 +42,6 @@ class MyDocument extends Document {
               a.appendChild(r);
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `,
-            }}
-          />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `
-              <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5T77DVZ"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
-            `,
             }}
           />
           <Main />
