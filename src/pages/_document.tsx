a4e7e5043028d7aac345cc57ece83e9a4a1cf981
sdeveloper123 <shubham.k8@turing.com>
2026-05-22 12:59:57 +0530

    Fixed tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,19 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           `,
             }}
           />
+          <Script
+            id='clarity'
+            strategy='afterInteractive'
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wuyvzg0yna");
+          `,
+            }}
+          />
           {/* Google Tag Manager (noscript) — GTM-5T77DVZ */}
           {/* <noscript
             dangerouslySetInnerHTML={{
