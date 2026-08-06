2d78080f17ff1849bdf9dea55921f8d033fd0530
pawanwitarist <pawan@witarist.com>
2026-08-06 13:06:56 +0530

    added: missing assest

--- src/components/header/headerData.tsx
+++ src/components/header/headerData.tsx
@@ -211,7 +211,7 @@ export const Resources = [
     name: 'Fertility Lexicon',
     description: 'Infertility and Fertility Care',
     href: '/resources/fertility-lexicon',
-    icon: 'https://res.cloudinary.com/garbhagudiivf/image/upload/v1782979161/Icons/Resources/fertility-lexicon_fqyuvm.png',
+    icon: 'https://ap-south-1.graphassets.com/ATvkR6mxuRke4HGT9LQrhz/cmsh7c6pb4ji206mrjr5af8ya',
   },
 ];
 
